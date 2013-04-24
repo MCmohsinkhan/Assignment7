3bcc41d6a6c8799002f1fbddaaa0af3013f84cd4
MCmohsinkhan <mkhan115@montgomerycollege.edu>
2013-04-24 14:44:15 -0300

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,9 +1,17 @@
 public class Driver {
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 
 		savingAccount a1 = new savingAccount(1001);
 
+		System.out.println(a1.toString());
+
+		a1.setBalance(101.0);
+		System.out.println("");
+		System.out.println(a1.toString());
+
+		a1.monthlyProcess();
+		System.out.println("");
 		System.out.println(a1.toString());
 	}
 }
