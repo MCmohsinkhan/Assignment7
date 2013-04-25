b3cd8000463d105c2c9bd9b44850e2dc8a770e0a
MCmohsinkhan <mkhan115@montgomerycollege.edu>
2013-04-25 11:52:57 -0300

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -7,11 +7,11 @@ public class Driver {
 		System.out.println(a1.toString());
 
 		a1.setBalance(101.0);
-		System.out.println("");
+		System.out.println("\nafter setBalance method");
 		System.out.println(a1.toString());
 
 		a1.monthlyProcess();
-		System.out.println("");
+		System.out.println("\nafter monthlyProcess");
 		System.out.println(a1.toString());
 	}
 }
